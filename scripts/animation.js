b70bba807233ecb392b31be23b14541387ed5652
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-21 17:08:05 +0200

    test

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,27 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const isMobile = window.innerWidth <= 768;
-    if (!isMobile) { 
-        const observerOptions = {
-            threshold: 0.1
-        };
+    const observerOptions = {
+        threshold: 0.1
+    };
 
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('show');
-                    observer.unobserve(entry.target);
-                }
-            });
-        }, observerOptions);
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
 
-        const elements = document.querySelectorAll('.fade-in');
-        elements.forEach(el => {
-            observer.observe(el);
-        });
-    } else {
-        const elements = document.querySelectorAll('.fade-in');
-        elements.forEach(el => {
-            el.classList.add('show');
-        });
-    }
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(el => {
+        observer.observe(el);
+    });
 });
