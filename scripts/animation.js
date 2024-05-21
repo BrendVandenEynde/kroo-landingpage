81e2d578a6e1a35cfad0aa2b36670f646251dc66
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-21 15:43:50 +0200

    style: add animation

---
+++ scripts/animation.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const observerOptions = {
+        threshold: 0.6
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(el => {
+        observer.observe(el);
+    });
+});
