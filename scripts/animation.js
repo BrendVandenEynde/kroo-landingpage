d1b204a77756e2ecf44f0eba0c74235ad30f8cd9
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-21 15:48:17 +0200

    fix: decrease threshold

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const observerOptions = {
-        threshold: 0.6
+        threshold: 0.4
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
